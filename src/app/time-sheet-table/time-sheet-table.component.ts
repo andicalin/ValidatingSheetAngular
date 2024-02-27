07b6806a22cf301e1b9fea0ce10709e4410cb173
andicalin <calinandiflorin@gmail.com>
2024-02-27 21:44:02 +0200

    Basic

---
+++ src/app/time-sheet-table/time-sheet-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-sheet-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './time-sheet-table.component.html',
+  styleUrl: './time-sheet-table.component.scss'
+})
+export class TimeSheetTableComponent {
+
+}
---
+++ src/app/time-sheet-upload/time-sheet-upload.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-sheet-upload',
+  standalone: true,
+  imports: [],
+  templateUrl: './time-sheet-upload.component.html',
+  styleUrl: './time-sheet-upload.component.scss'
+})
+export class TimeSheetUploadComponent {
+
+}
